785c9c49ac4fa77948f3d0878bd9ec2859d56c58
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-02-20 14:54:41 +0300

    win rules

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'scissor'];
+    const choices = ['Rock', 'Paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    // make it case-sensitive 
+    playerSelection = playerSelection.toLowercase
+
+    //tie
+    if (playerSelection === computerSelection) {
+        return "It is a tie!";
+    }else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
 }
