989be48b930ed0e6d9daafcce66cf7a9389abd27
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-02-20 15:08:17 +0300

    results

--- index.js
+++ index.js
@@ -21,3 +21,31 @@ function playRound(playerSelection, computerSelection) {
         return `You lose! ${computerSelection} beats ${playerSelection}`
     }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 1; i <= 5; i++) {
+      const playerSelection = prompt(`Round ${i}: Enter Rock, Paper, or Scissors`);
+      const computerSelection = getComputerChoice();
+  
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes('win')) {
+        playerScore++;
+      } else if (result.includes('lose')) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log(`You win the game! Final score: ${playerScore} - ${computerScore}`);
+    } else if (computerScore > playerScore) {
+      console.log(`You lose the game! Final score: ${playerScore} - ${computerScore}`);
+    } else {
+      console.log(`It's a tie! Final score: ${playerScore} - ${computerScore}`);
+    }
+  }
+  
