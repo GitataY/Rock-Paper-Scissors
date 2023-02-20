90ddd3a0cff7219ddef4a3ac16031a8a64639e72
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-02-20 14:42:14 +0300

    computer choice

---
+++ index.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'scissor'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
